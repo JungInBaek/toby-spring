0e4abc8d3333f529fa914d45e6f8790c56bbe628
JungInBaek <bji1527@naver.com>
2021-12-10 16:59:09 +0900

    라이브러리 위치 변경, 테스트 개선
    main() 테스트에서 JUnit 테스트로 전환

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -1,35 +1,59 @@
 package springbook.user.dao;
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class UserDaoTest {
 
-    //  테스트
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-//        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-//        UserDao dao = context.getBean("userDao", UserDao.class);
-
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+    @Test
+    public void addAndGet() throws SQLException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
-        user.setId("whiteship");
-        user.setName("백기선");
-        user.setPassword("married");
+        user.setId("gyumee");
+        user.setName("박성철");
+        user.setPassword("springno1");
 
+        //  등록
         dao.add(user);
 
-        System.out.println(user.getId() + " 등록 성공");
-
+        //  조회
         User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
 
-        System.out.println(user2.getId() + " 조회 성공");
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
     }
+
+    //  테스트
+//    public static void main(String[] args) throws SQLException {
+//        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+//        UserDao dao = context.getBean("userDao", UserDao.class);
+//
+//        User user = new User();
+//        user.setId("whiteship");
+//        user.setName("백기선");
+//        user.setPassword("married");
+//
+//        //  등록
+//        dao.add(user);
+//        System.out.println(user.getId() + " 등록 성공");
+//
+//        //  조회
+//        User user2 = dao.get(user.getId());
+//        if(!user.getName().equals(user2.getName())) {
+//            System.out.println("테스트 실패 (name)");
+//        } else if(!user.getPassword().equals(user2.getPassword())) {
+//            System.out.println("테스트 실패 (password)");
+//        } else {
+//            System.out.println("조회 테스트 성공");
+//        }
+//    }
 }

83ae6992f146c600ee777bf04b133382db7f70f7
JungInBaek <bji1527@naver.com>
2021-12-27 01:13:08 +0900

    UserTest 생성, UserServiceTest 개선
    User의 upgradeLevel()에 대한 테스트 생성
    UserServiceTest2의 checkLevel() -> checkLevelUpgraded()로 개선
    UserService와 UserServiceTest2에 중복되는 상수 중복 제거

--- src/springbook/user/service/UserService.java
+++ src/springbook/user/service/UserService.java
@@ -44,11 +44,14 @@ public class UserService {
         }
     }
 
+    public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
+    public static final int MIN_RECOMMEND_FOR_GOLD = 30;
+
     private boolean canUpgradeLevel(User user) {
         Level currentLevel = user.getLevel();
         switch(currentLevel) {
-            case BASIC: return (user.getLogin() >= 50);
-            case SILVER: return (user.getRecommend() >= 30);
+            case BASIC: return (user.getLogin() >= MIN_LOGCOUNT_FOR_SILVER);
+            case SILVER: return (user.getRecommend() >= MIN_RECOMMEND_FOR_GOLD);
             case GOLD: return false;
             default: throw new IllegalArgumentException("Unknown Level : " + currentLevel);
         }
--- src/springbook/user/service/UserServiceTest2.java
+++ src/springbook/user/service/UserServiceTest2.java
@@ -15,6 +15,8 @@ import java.util.List;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static springbook.user.service.UserService.MIN_LOGCOUNT_FOR_SILVER;
+import static springbook.user.service.UserService.MIN_RECOMMEND_FOR_GOLD;
 
 
 //  컨테이너에 종속적이지 않은 테스트
@@ -29,11 +31,11 @@ public class UserServiceTest2 {
     @Before
     public void setUp() {
         users = Arrays.asList(
-                new User("bumjin", "박범진", "p1", Level.BASIC, 49, 0),
-                new User("joytouch", "강명성", "p2", Level.BASIC, 50, 0),
-                new User("erwins", "신승한", "p3", Level.SILVER, 60, 29),
-                new User("madnite1", "이상호", "p4", Level.SILVER, 60, 30),
-                new User("green", "오민규", "p5", Level.GOLD, 100, 100)
+                new User("bumjin", "박범진", "p1", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER-1, 0),
+                new User("joytouch", "강명성", "p2", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER, 0),
+                new User("erwins", "신승한", "p3", Level.SILVER, MIN_RECOMMEND_FOR_GOLD-1, 29),
+                new User("madnite1", "이상호", "p4", Level.SILVER, MIN_RECOMMEND_FOR_GOLD, 30),
+                new User("green", "오민규", "p5", Level.GOLD, 100, Integer.MAX_VALUE)
         );
 
         dataSource = new SingleConnectionDataSource("jdbc:h2:tcp://localhost/~/testdb", "spring", "book", true);
@@ -57,17 +59,26 @@ public class UserServiceTest2 {
 
         userService.upgradeLevels();
 
-        checkLevel(users.get(0), Level.BASIC);
-        checkLevel(users.get(1), Level.SILVER);
-        checkLevel(users.get(2), Level.SILVER);
-        checkLevel(users.get(3), Level.GOLD);
-        checkLevel(users.get(4), Level.GOLD);
+        checkLevelUpgraded(users.get(0), false);
+        checkLevelUpgraded(users.get(1), true);
+        checkLevelUpgraded(users.get(2), false);
+        checkLevelUpgraded(users.get(3), true);
+        checkLevelUpgraded(users.get(4), false);
     }
 
-    private void checkLevel(User user, Level expectedLevel) {
+    private void checkLevelUpgraded(User user, boolean upgraded) {
         User userUpdate = userDao.get(user.getId());
-        assertThat(userUpdate.getLevel(), is(expectedLevel));
+        if(upgraded) {
+            assertThat(userUpdate.getLevel(), is(user.getLevel().nextLevel()));
+        } else {
+            assertThat(userUpdate.getLevel(), is(user.getLevel()));
+        }
     }
+
+//    private void checkLevel(User user, Level expectedLevel) {
+//        User userUpdate = userDao.get(user.getId());
+//        assertThat(userUpdate.getLevel(), is(expectedLevel));
+//    }
 
     @Test
     public void add() {
